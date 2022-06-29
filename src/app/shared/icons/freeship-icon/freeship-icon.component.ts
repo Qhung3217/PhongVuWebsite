9625c4844ae7806aa9a5f87b259440feb44d0ce0
Qhung3217 <quanghung165200@gmail.com>
2022-06-29 18:12:22 +0700

    add section-hot-deal, deal-table component and product component

---
+++ src/app/shared/icons/freeship-icon/freeship-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-freeship-icon',
+  templateUrl: './freeship-icon.component.html',
+})
+export class FreeshipIconComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
