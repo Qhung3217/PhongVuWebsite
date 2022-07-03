3b26fef3c897d1637bb7ab9b0fe98b74f2ba09c0
Qhung3217 <quanghung165200@gmail.com>
2022-07-03 17:22:16 +0700

    complete html/css detail page

---
+++ src/app/shared/icons/arrow-down-icon/arrow-down-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arrow-down-icon',
+  templateUrl: './arrow-down-icon.component.html',
+})
+export class ArrowDownIconComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
